dade3549901d19729af0c7fdc8d1fba82e51adbe
A-asava <adikasaid996@gmail.com>
2024-07-01 10:41:12 +0000

    deploy_web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo  mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+      Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+printf "%s\n" "server {
+     listen 80 default_server;
+     listen [::]:80 default_server;
+     add_header X-Served-By \$HOSTNAME;
+     root /var/www/html;
+     index index.html index.htm;
+     location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+     }
+     location /redirect_me {
+	return 301 https://github.com/A-asava;
+     }
+     error_page 404 /404.html;
+     location /404 {
+	root /var/www/html;
+	internal;
+     }
+}" | sudo tee /etc/nginx/sites-available/default
+
+sudo service nginx restart
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Fabric script that distributes an archive to your web
+    servers, using the function do_deploy:
+"""
+from fabric.api import run, put, env
+import os
+env.hosts = ['54.174.84.60', '100.25.31.7']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_deploy(archive_path):
+    """Returns False if the file at the path archive_path
+        doesnt exist
+    Args:
+        archive_path: path
+    """
+
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        archive = archive_path.split('/')[-1]
+        archive_no_ext = archive.split('.')[0]
+        remote_path = "/tmp/{}".format(archive)
+        remote_folder = "/data/web_static/releases/{}/".format(archive_no_ext)
+
+        put(archive_path, remote_path)
+        run("sudo mkdir -p {}".format(remote_folder))
+        run("sudo tar -xzf {} -C {}".format(remote_path, remote_folder))
+        run("sudo rm {}".format(remote_path))
+        run("sudo mv {0}web_static/* {0}".format(remote_folder))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(remote_folder))
+
+        return True
+    except Exception:
+        return False
